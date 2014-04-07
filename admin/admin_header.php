a03f1fb59efda1af1a1c786035713ed96fdecb68
impresscms <contact@impresscms.org>
2014-04-07 02:02:12 +0000

    Added missing file to show the about page.
    git-svn-id: http://amaryllis-modules.googlecode.com/svn/trunk/ccenter@1090 66ca2e0c-c8bd-fb7b-0ff6-659cc54a85ad

---
+++ admin/admin_header.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Admin header file
+ *
+ * This file is included in all pages of the admin side and being so, it proceeds to a few
+ * common things.
+ *
+ * @copyright	Copyright sato-san (Rene Sato)
+ * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+ * @since		1.0
+ * @author		sato-san (Rene Sato) <sato-san@impresscms.org>
+ * @package		cms
+ * @version		$sato-san$
+*/
+
+include_once "../../../include/cp_header.php";
+include_once ICMS_ROOT_PATH . "/modules/" . basename(dirname(dirname(__FILE__))) . "/include/common.php";
+if (!defined("CMS_ADMIN_URL")) define("CMS_ADMIN_URL", CMS_URL . "admin/");
+include_once CMS_ROOT_PATH . "include/requirements.php";
